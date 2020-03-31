057ee9978db9f4fe63d9f0b6daa3c6f2b3ba692c
krzosa <krzosa.karol@gmail.com>
2020-03-31 11:03:28 +0200

    Add routing form /schemas, /schemas/hash

--- main.py
+++ main.py
@@ -10,8 +10,19 @@ def hello():
     return openapi
 
 
+@app.route("/schemas", methods = ["POST"])
+def addSchema():
+    print(request.form)
+    return "data has been added to repo"
+
+
+@app.route("/schemas/<schemaHash>")
+def getSchema(schemaHash):
+    return "successful operation"
+
+
 def main():
-    app.run(debug=True)
+    app.run(port=9292)
 
 
 if __name__ == "__main__":
